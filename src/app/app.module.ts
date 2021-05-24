ea5272069b46b93a0b647f207e594cf0665805be
wesleyrcastro <wesleyrcastro@yahoo.com.br>
2021-05-23 23:54:56 -0300

    Implementação parcial da categoria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
 import { HomeComponent } from './components/views/home/home.component';
 import {MatCardModule} from '@angular/material/card';
+import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
+import {MatTableModule} from '@angular/material/table';
 
 
 @NgModule({
@@ -22,7 +24,8 @@ import {MatCardModule} from '@angular/material/card';
     HeaderComponent,
     FooterComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    CategoriaReadComponent
   ],
     imports: [
         BrowserModule,
@@ -32,7 +35,8 @@ import {MatCardModule} from '@angular/material/card';
         MatSidenavModule,
         MatListModule,
         MatIconModule,
-        MatCardModule
+        MatCardModule,
+        MatTableModule
 
     ],
   providers: [],
